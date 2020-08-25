824dfa19024ab22c1dbf83fefe47f88544ee970c
timlaurijs <timlaurijs@gmail.com>
2020-08-25 11:26:12 +0200

    relacate cors middleware

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-const express = require("express");
-const { PORT } = require("./config/constants");
+const express = require("express")
+const { PORT } = require("./config/constants")
 //Auth
-const loggerMiddleWare = require("morgan");
-const authMiddleware = require("./auth/middleware");
-const corsMiddleWare = require("cors");
+const loggerMiddleWare = require("morgan")
+const authMiddleware = require("./auth/middleware")
+const corsMiddleWare = require("cors")
 
 //Routers
-const userRoute = require("./routers/users");
-const authRouter = require("./routers/auth");
-const resultRoute = require("./routers/result");
-const exerciseRoute = require("./routers/exercises");
+const userRoute = require("./routers/users")
+const authRouter = require("./routers/auth")
+const resultRoute = require("./routers/result")
+const exerciseRoute = require("./routers/exercises")
 
 //Models
-const User = require("./models").user;
+const User = require("./models").user
 
-const app = express();
+const app = express()
+app.use(corsMiddleWare())
 
-const bodyParserMiddleWare = express.json();
+const bodyParserMiddleWare = express.json()
 
-app.use(loggerMiddleWare("dev"));
-app.use(bodyParserMiddleWare);
+app.use(loggerMiddleWare("dev"))
+app.use(bodyParserMiddleWare)
 
 // Routes
-app.use("/", authRouter);
-app.use("/users", userRoute);
-app.use("/result", resultRoute);
-app.use("/exercise", exerciseRoute); //NOT WORKING
-
-app.use(corsMiddleWare());
+app.use("/", authRouter)
+app.use("/users", userRoute)
+app.use("/result", resultRoute)
+app.use("/exercise", exerciseRoute) //NOT WORKING
 
 if (process.env.DELAY) {
   app.use((req, res, next) => {
-    setTimeout(() => next(), parseInt(process.env.DELAY));
-  });
+    setTimeout(() => next(), parseInt(process.env.DELAY))
+  })
 }
 
 //Start a simple express server:
@@ -48,5 +47,5 @@ if (process.env.DELAY) {
 // });
 
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+  console.log(`Listening on port: ${PORT}`)
+})
