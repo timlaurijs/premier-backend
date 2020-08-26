b823e7c9fab642cb973598674b60f64e38a8c3c4
Karem1986 <karem.ortiz@outlook.com>
2020-08-26 14:20:56 +0200

    added auth signup image and descrption

--- routers/auth.js
+++ routers/auth.js
@@ -39,9 +39,9 @@ router.post("/login", async (req, res, next) => {
 });
 
 router.post("/signup", async (req, res) => {
-  const { email, password, name } = req.body;
+  const { email, password, name, imageUrl, description } = req.body;
 
-  if (!email || !password || !name) {
+  if (!email || !password || !name || imageUrl || description) {
     return res.status(400).send("Please provide an email, password and a name");
   }
 
@@ -50,6 +50,8 @@ router.post("/signup", async (req, res) => {
       email,
       password: bcrypt.hashSync(password, 10),
       name,
+      imageUrl,
+      description,
     });
 
     delete newUser.dataValues["password"]; // don't send back the password hash
